41562d6586233eb0998abb62272d35fb8683e409
Vera-Galenko <vera.galenko@icloud.com>
2020-09-21 17:10:03 +0000

    letter by letter

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,11 @@
-const sentence = "hello there from lighthouse labs";
-
+const sentence = "hello there from lighthouse labs \n";
+let n = 0;
 for (const char of sentence) {
+    n += 1000;
     setTimeout(() => {
         process.stdout.write(char);
-      }, 1000)
-    
+      }, 1000 + n);
   };
+  // console.log('\n');
 
 
-//   for (const char of sentence) {
-//     console.log(char);
-//   }
